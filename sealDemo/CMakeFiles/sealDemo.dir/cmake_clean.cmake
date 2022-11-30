041af487360d570a6e50ced334d498f81d36a521
Jalope <60742112+Jalope@users.noreply.github.com>
2022-11-30 08:51:16 +0100

    act

---
+++ sealDemo/CMakeFiles/sealDemo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sealDemo.dir/sealDemo.cpp.o"
+  "CMakeFiles/sealDemo.dir/sealDemo.cpp.o.d"
+  "sealDemo"
+  "sealDemo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sealDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
